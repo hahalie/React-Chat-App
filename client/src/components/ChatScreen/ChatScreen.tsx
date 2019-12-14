a92e2e243826b12c698b508e8fec5680340239d2
hahalie <tereuspark@yahoo.com>
2019-12-14 14:42:25 -0600

    Update Chat Screen & Join Screen
    Update Chat Screen & Join Screen || Socket.io integration for client

--- client/src/components/ChatScreen/ChatScreen.tsx
+++ client/src/components/ChatScreen/ChatScreen.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import { RouteComponentProps } from "react-router-dom";
 
-const ChatScreen: React.FC = () => {
-    return (
-        <div>
-            Chat Screen Component
-        </div>
-    )
-}
+interface Props {}
+
+let socket: any;
+
+const ChatScreen: React.FC<Props> = ({ location }: RouteComponentProps) => {
+  const [name, setName] = useState<string | string[]>("");
+  const [room, setRoom] = useState<string | string[]>("");
+
+  const ENDPOINT: string = "localhost:5000";
+
+  useEffect(() => {
+    const { name, room }: queryString.ParsedQuery<string> = queryString.parse(
+      location.search
+    );
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
+
+  return (
+    <div className="chatScreenOuterContainer">
+      <div className="chatScreenInnerContainer">
+        This is {name}'s Chat Screen in room #{room}
+      </div>
+    </div>
+  );
+};
 
 export default ChatScreen;
--- client/src/components/JoinScreen/JoinScreen.tsx
+++ client/src/components/JoinScreen/JoinScreen.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
- 
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const JoinScreen: React.FC = () => {
-    return (
+interface Props {}
+
+const JoinScreen: React.FC<Props> = () => {
+  const [name, setName] = useState<string | string[]>("");
+  const [room, setRoom] = useState<string | string[]>("");
+
+  return (
+    <div className="joinScreenOuterContainer">
+      <div className="joinScreenInnerContainer">
+        <h1 className="heading">Let's Chat</h1>
         <div>
-            Join Screen Component
+          <input
+            type="text"
+            placeholder="Name"
+            className="screenInput"
+            onChange={event => setName(event.target.value)}
+          />
         </div>
-    )
-}
+        <div>
+          <input
+            type="text"
+            placeholder="Room"
+            className="screenInput mt-20"
+            onChange={event => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          to={`/chat?name=${name}&room=${room}`}
+          onClick={event => (!name || !room ? event.preventDefault() : null)}
+        >
+          <button className="signInButton mt-20" type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default JoinScreen;
