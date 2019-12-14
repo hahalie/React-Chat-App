5e6eb186dc4336ad852d6e8215cb4c10e444174d
hahalie <tereuspark@yahoo.com>
2019-12-14 09:09:20 -0600

    Create users.ts
    Create User Helpers

---
+++ server/users.ts
@@ -0,0 +1,34 @@
+const users: any = []
+
+const addUser = ({ id, name, room }:any) => {
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    const existingUser = users.find((user:any) => {
+        user.room === room && user.name === name
+    })
+
+    if(existingUser) {
+        return { error: `Username is taken`}
+    }
+
+    const user = { id, name, room };
+
+    users.push(user)
+
+    return { user }
+}
+
+const removeUser = (id:any) => {
+    const index:any = users.findIndex((user:any) => users.id === id);
+
+    if(index !== -1) {
+        return users.splice(index, 1)[0];
+    }
+}
+
+const getUser = (id:any) => users.find((user:any) => user.id === id)
+
+const getUsersInRoom = (room:any) => users.filter((user:any) => user.room === room)
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }
