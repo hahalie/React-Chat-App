2fcac2e5fcaee58ca1df32b2b2af8ebfaa3788af
hahalie <tereuspark@yahoo.com>
2019-12-14 09:16:39 -0600

    Update index.ts (Socket.io Integration)
    Update main file || Socket.io Integration is added to be ready to communicate with client in realtime.

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,10 @@ import express, { Application } from "express";
 
 import http from "http";
 
+import socketio from "socket.io";
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
 const cors = require("cors");
 
 const router: any = require("./router");
@@ -14,6 +18,56 @@ app.use(router);
 app.use(cors);
 
 const server: http.Server = http.createServer(app);
+
+const io: socketio.Server = socketio(server);
+
+io.on("connection", (socket: socketio.Socket) => {
+    socket.on("join", ({ name, room }:any, callback: any) => {
+      const { error, user } = addUser({ id: socket.id, name, room });
+  
+      if (error) return callback(error);
+  
+      socket.emit("message", {
+        user: "admin",
+        text: `${user.name}, welcome to the room #${user.room}`
+      });
+      socket.broadcast
+        .to(user.room)
+        .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+  
+      socket.join(user.room);
+  
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+  
+      callback();
+    });
+  
+    socket.on("sendMessage", (message, callback) => {
+      const user = getUser(socket.id);
+  
+      io.to(user.room).emit("message", { user: user.name, text: message });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+  
+      callback();
+    });
+  
+    socket.on("disconnect", () => {
+      const user = removeUser(socket.id);
+  
+      if (user) {
+        io.to(user.room).emit("message", {
+          user: "admin",
+          text: `${user.name} has left.`
+        });
+      }
+    });
+  });
 
 
 server.listen(PORT, () => console.log(`Server is started on ${PORT}`));
